ca39b024b6420adea16c192abac4994f5d42942f
Nick Melanson <nickm890@gmail.com>
2022-09-19 15:19:20 -0400

    initial torrent setup, bencode installed to parse data

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+const fs = require("fs");
+const bencode = require("bencode");
+
+const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
+console.log(torrent.announce.toString("utf8"));
