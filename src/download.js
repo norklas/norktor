5fd82a39fdf6c96b2d8fbfdfc92d07e875701e68
Nick Melanson <nickm890@gmail.com>
2022-09-21 14:41:38 -0400

    added nonhandshake handlers

--- src/download.js
+++ src/download.js
@@ -38,7 +38,17 @@ function onWholeMsg(socket, callback) {
 }
 
 function msgHandler(msg, socket) {
-  if (isHandshake(msg)) socket.write(message.buildInterested());
+  if (isHandshake(msg)) {
+    socket.write(message.buildInterested());
+  } else {
+    const m = message.parse(msg);
+
+    if (m.id === 0) chokeHandler();
+    if (m.id === 1) unchokeHandler();
+    if (m.id === 4) haveHandler(m.payload);
+    if (m.id === 5) bitfieldHandler(m.payload);
+    if (m.id === 7) pieceHandler(m.payload);
+  }
 }
 
 function isHandshake(msg) {
@@ -47,3 +57,13 @@ function isHandshake(msg) {
     msg.toString("utf8", 1, 20) === "BitTorrent protocol"
   );
 }
+
+function chokeHandler() {}
+
+function unchokeHandler() {}
+
+function haveHandler(payload) {}
+
+function bitfieldHandler(payload) {}
+
+function pieceHandler(payload) {}
